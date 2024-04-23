9b6e7948afbd7a73aa0c6c9b8663e0c68919b4d9
Finn Archer <finnharcher@gmail.com>
2024-04-23 16:33:52 -0500

    Added Initial Folders

---
+++ contactUs/contactUs.js
@@ -0,0 +1,12 @@
+const contacts=document.getElementById("contacts")
+function newContact(position,name,number) {
+    let newContact=document.createElement("li")
+    newContact.innerHTML=`${position}: ${name} - <a href="mailto:${number}">${number}@amaisd.net</a>`
+    contacts.appendChild(newContact);
+}
+function newContacts(infoLists) {
+    infoLists.forEach(infoList => {
+        newContact(infoList[0],infoList[1],infoList[2])
+    });
+}
+newContacts([["President","Veditha Chelikani","309920"],["Treasurer","Zainab Adnan","267918"],["Secretary","Dakota",""],["Head Manager of Technology and Communications","Finn Archer","277341"]])
