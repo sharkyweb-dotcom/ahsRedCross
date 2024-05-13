192b72666f7fc91f4acd85a93cd548e3cf79235f
sharkyweb-dotcom <finnharcher@gmail.com>
2024-05-13 17:33:38 -0500

    Generalized positions and removed technology manager

--- contactUs/contactUs.js
+++ contactUs/contactUs.js
@@ -11,8 +11,8 @@ function newContacts(infoLists) {
 }
 newContacts([
     ["President","Veditha Chelikani","309920"],
-    ["Treasurer","Zainab Adnan","267918"],
-    ["Secretary","Dakota Chavez","268366"],
-    ["Historian","Sanjana Gunda","272781"],
-    ["Head Manager of Technology and Communications","Finn Archer","277341"]
+    ["Officer","Zainab Adnan","267918"],
+    ["Officer","Dakota Chavez","268366"],
+    ["Officer","Sanjana Gunda","272781"],
+    // ["Head Manager of Technology and Communications","Finn Archer","277341"]
 ])
